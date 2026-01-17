d418abf56aa7b2c9738ff05d00661be3d930448e
Fayeq Bahram <fayeq.bahram@gmail.com>
2026-01-17 23:52:15 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.getElementById("navLinks");
-
-menuToggle.addEventListener("click", ()=>{
-   navLinks.classList.toggle("new-link");
-   console.log("hello")
-})
-
-
